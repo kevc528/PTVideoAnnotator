b08e3a438352429cda74f134cf23ab26da5d5782
Kevin Chen <kevc528@seas.upenn.edu>
2022-04-18 05:05:50 -0400

    notes frontend

--- frontend/static/js/annotate.js
+++ frontend/static/js/annotate.js
@@ -2,6 +2,49 @@
 
 const VIDEO_DIR = "../static/videos/"
 
+var events = {};
+
+function redisplayNotes(video) {
+
+    function str_pad_left(string, pad, length) {
+        return (new Array(length + 1).join(pad) + string).slice(-length);
+    }
+
+    var notesList = document.getElementById("note-list");
+
+    while (notesList.firstChild) {
+        notesList.removeChild(notesList.firstChild);
+    }
+
+    var notes = [];
+    for (var timestamp in events) {
+        events[timestamp].forEach(e => {
+            if (e.type === "note") {
+                notes.push({ "timestamp": timestamp, "content": e.content })
+            }
+        })
+    }
+    notes.sort((a, b) => a.timestamp - b.timestamp);
+
+    notes.forEach(note => {
+        var li = document.createElement('li');
+
+        var minutes = Math.floor(note.timestamp / 60);
+        var seconds = Math.floor(note.timestamp - minutes * 60);
+
+        var finalTime = str_pad_left(minutes, '0', 2) + ':' + str_pad_left(seconds, '0', 2);
+
+        li.innerHTML = `${finalTime}: ${note.content}`;
+        li.classList.add('note-item');
+
+        li.onclick = function () {
+            video.currentTime = note.timestamp;
+        }
+
+        notesList.appendChild(li);
+    })
+}
+
 window.addEventListener("load", function () {
     var video = document.getElementById('video');
     var source = document.createElement('source');
@@ -18,8 +61,22 @@ window.addEventListener("load", function () {
 
     var addNoteButton = document.getElementById('note-button');
     addNoteButton.onclick = function () {
+        if (noteTextArea.value === "") {
+            alert("Empty note!");
+            return;
+        }
         var noteContent = noteTextArea.value;
-        console.log(noteContent);
+        var videoTime = video.currentTime;
+        var noteEvent = { "type": "note", "content": noteContent };
+
+        if (videoTime in events) {
+            events[videoTime].push(noteEvent);
+        } else {
+            events[videoTime] = [noteEvent];
+        }
+
+        redisplayNotes(video);
+
         noteTextArea.value = "";
     }
 })
