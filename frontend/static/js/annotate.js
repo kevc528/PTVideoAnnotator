c6cd81ba496ccb58e584e9988b90d603df123011
Kevin Chen <kevc528@seas.upenn.edu>
2022-05-04 18:41:03 -0400

    refactor

--- frontend/static/js/annotate.js
+++ frontend/static/js/annotate.js
@@ -2,93 +2,18 @@
 
 const VIDEO_DIR = "../static/videos/"
 
-var events = {};
-
-function redisplayNotes(video) {
-
-    function str_pad_left(string, pad, length) {
-        return (new Array(length + 1).join(pad) + string).slice(-length);
-    }
-
-    var notesList = document.getElementById("note-list");
-
-    while (notesList.firstChild) {
-        notesList.removeChild(notesList.firstChild);
-    }
-
-    var notes = [];
-    for (var timestamp in events) {
-        events[timestamp].forEach(e => {
-            if (e.type === "note") {
-                notes.push({ "timestamp": timestamp, "content": e.content })
-            }
-        })
-    }
-    notes.sort((a, b) => a.timestamp - b.timestamp);
-
-    notes.forEach(note => {
-        var li = document.createElement('li');
-
-        var minutes = Math.floor(note.timestamp / 60);
-        var seconds = Math.floor(note.timestamp - minutes * 60);
-
-        var finalTime = str_pad_left(minutes, '0', 2) + ':' + str_pad_left(seconds, '0', 2);
-
-        li.innerHTML = `${finalTime}: ${note.content}`;
-        li.classList.add('note-item');
-
-        li.onclick = function () {
-            video.pause()
-            video.currentTime = note.timestamp;
-        }
-
-        notesList.appendChild(li);
-    })
-}
-
 window.addEventListener("load", function () {
     var video = document.getElementById('video');
     var source = document.createElement('source');
-
     source.setAttribute('src', VIDEO_DIR + this.window.location.pathname.split('/').pop() + '.mp4');
     source.setAttribute('type', 'video/mp4');
-
     video.appendChild(source);
 
-    var noteTextArea = document.getElementById('note-textarea');
-    noteTextArea.onfocus = function () {
-        video.pause();
-    }
-
-    var addNoteButton = document.getElementById('note-button');
-    addNoteButton.onclick = function () {
-        if (noteTextArea.value === "") {
-            alert("Empty note!");
-            return;
-        }
-        var noteContent = noteTextArea.value;
-        var videoTime = video.currentTime;
-        var noteEvent = { "type": "note", "content": noteContent };
-
-        if (videoTime in events) {
-            events[videoTime].push(noteEvent);
-        } else {
-            events[videoTime] = [noteEvent];
-        }
-
-        redisplayNotes(video);
-
-        noteTextArea.value = "";
-    }
-
-    var playButton = document.getElementById('play-button');
-    playButton.onclick = function () {
-        video.play();
-    }
-    var pauseButton = document.getElementById('pause-button');
-    pauseButton.onclick = function () {
-        video.pause();
-    }
+    var videoDouble = document.getElementById('video-double');
+    var sourceDouble = document.createElement('source');
+    sourceDouble.setAttribute('src', VIDEO_DIR + this.window.location.pathname.split('/').pop() + '.mp4');
+    sourceDouble.setAttribute('type', 'video/mp4');
+    videoDouble.appendChild(sourceDouble);
 
     var durationSlider = document.getElementById('duration-slider');
     var durationLabel = document.getElementById('duration-label');
@@ -96,28 +21,26 @@ window.addEventListener("load", function () {
 
     durationSlider.value = "0";
 
-    durationSlider.addEventListener('change', (event) => {
-        var seconds = parseFloat(event.target.value) / 100 * 5;
-        durationLabel.innerText = `Duration: ${Math.round(seconds * 10) / 10} seconds`
-
-        let currVideoTime = video.currentTime;
-        video.currentTime = currVideoTime + seconds;
-
-        var oldOnPlay = video.onplay;
-
-        video.onpause = function () {
-            video.onplay = oldOnPlay;
-            video.onpause = null;
-        }
-
-        video.onplay = function () {
-            video.pause();
-            durationCanvas.getContext('2d').drawImage(video, 0, 0, durationCanvas.width, durationCanvas.height);
-        }
-
+    var playButton = document.getElementById('play-button');
+    playButton.onclick = function () {
+        durationCanvas.getContext('2d').clearRect(0, 0, durationCanvas.width, durationCanvas.height);
         video.play();
+    }
+    var pauseButton = document.getElementById('pause-button');
+    pauseButton.onclick = function () {
+        video.pause();
+    }
 
-        video.currentTime = currVideoTime;
+    durationSlider.addEventListener('change', (event) => {
+        if (video.paused) {
+            var seconds = parseFloat(event.target.value) / 100 * 5;
+            durationLabel.innerText = `Duration: ${Math.round(seconds * 10) / 10} seconds`
+            videoDouble.currentTime = video.currentTime + seconds;
+            videoDouble.play().then(_ => {
+                videoDouble.pause();
+                durationCanvas.getContext('2d').drawImage(videoDouble, 0, 0, durationCanvas.width, durationCanvas.height);
+            })
+        }
     });
 })
 
--- frontend/static/js/paint.js
+++ frontend/static/js/paint.js
@@ -1,6 +1,4 @@
 var color = "red";
-var myPath;
-var otherPath;
 var mouseDownPoint;
 
 window.onload = function () {
@@ -56,4 +54,87 @@ window.onload = function () {
             }
         })();
     }, 0);
+
+    function redisplayNotes(video) {
+
+        function str_pad_left(string, pad, length) {
+            return (new Array(length + 1).join(pad) + string).slice(-length);
+        }
+
+        var notesList = document.getElementById("note-list");
+
+        while (notesList.firstChild) {
+            notesList.removeChild(notesList.firstChild);
+        }
+
+        var notes = [];
+        for (var timestamp in events) {
+            events[timestamp].forEach(e => {
+                if (e.type === "note") {
+                    notes.push({ "timestamp": timestamp, "content": e.content })
+                }
+            })
+        }
+        notes.sort((a, b) => a.timestamp - b.timestamp);
+
+        notes.forEach(note => {
+            var li = document.createElement('li');
+
+            var minutes = Math.floor(note.timestamp / 60);
+            var seconds = Math.floor(note.timestamp - minutes * 60);
+
+            var finalTime = str_pad_left(minutes, '0', 2) + ':' + str_pad_left(seconds, '0', 2);
+
+            li.innerHTML = `${finalTime}: ${note.content}`;
+            li.classList.add('note-item');
+
+            li.onclick = function () {
+                video.currentTime = note.timestamp;
+                video.pause();
+                while (!video.paused) { }
+
+                video.play().then(_ => {
+                    video.pause();
+                    var tempCanvas = document.createElement("canvas");
+                    tempCanvas.width = canvas.width;
+                    tempCanvas.height = canvas.height;
+                    tempCanvas.getContext('2d').drawImage(video, 0, 0, tempCanvas.width, tempCanvas.height);
+                    var dataURL = tempCanvas.toDataURL();
+
+                    videoLayer.activate();
+                    var raster = new paper.Raster(dataURL);
+                    raster.position = paper.view.center;
+                    drawLayer.activate();
+                });
+            }
+
+            notesList.appendChild(li);
+        })
+    }
+
+    var noteTextArea = document.getElementById('note-textarea');
+    noteTextArea.onfocus = function () {
+        video.pause();
+    }
+
+    var addNoteButton = document.getElementById('annotation-button');
+    addNoteButton.onclick = function () {
+        if (noteTextArea.value === "") {
+            alert("Empty note!");
+            return;
+        }
+        var noteContent = noteTextArea.value;
+        var videoTime = video.currentTime;
+        var noteEvent = { "type": "note", "content": noteContent };
+
+        if (videoTime in events) {
+            events[videoTime].push(noteEvent);
+        } else {
+            events[videoTime] = [noteEvent];
+        }
+
+        redisplayNotes(video);
+
+        noteTextArea.value = "";
+    }
 }
