36fc00331c1a558c50a0a688555af59ce63bbe66
Kevin Chen <kevc528@seas.upenn.edu>
2022-05-05 19:19:15 -0400

    patient view

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 import os
 
@@ -6,13 +6,30 @@ TEMPLATE_DIR = os.path.abspath('../frontend/templates')
 STATIC_DIR = os.path.abspath('../frontend/static')
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
+video_events = {}
+
 @app.route('/')
 def main():
-    return "Hello"
+    return "<h1>Sent to patient</h1>"
 
-@app.route('/annotate/<filename>')
+@app.route('/annotate/<filename>', methods=['GET', 'POST'])
 def annotate(filename):
-    return render_template("annotate.html")
+    if request.method == "GET":
+        return render_template("annotate.html")
+    else:
+        body_json = request.get_json()
+        video_events[filename] = body_json
+        return redirect('/')
+
+@app.route('/watch/<filename>')
+def watch(filename):
+    return render_template("watch.html")
+
+@app.route('/events/<filename>')
+def get_events(filename):
+    events = video_events[filename] if filename in video_events else []
+    return {"events": events}
+
 
 if __name__ == "__main__":
     app.run(port=8000)
